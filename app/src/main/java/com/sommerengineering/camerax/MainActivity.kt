7bb49ec684f3d6db5e2100b3322e652d69b6759b
Drew Sommer <drew@sommerengineering.com>
2025-02-24 17:27:57 +0100

    feat: Request camera permission

--- app/src/main/java/com/sommerengineering/camerax/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/camerax/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.sommerengineering.camerax
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,12 +15,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.sommerengineering.camerax.ui.theme.CameraXTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // ensure camera permission is granted
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            cameraPermissionRequest.launch(android.Manifest.permission.CAMERA)
+        }
+
         setContent {
             CameraXTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
