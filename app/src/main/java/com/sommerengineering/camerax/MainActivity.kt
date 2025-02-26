fa160880e1009452f61c29ce8098f528f953b5a7
Drew Sommer <drew@sommerengineering.com>
2025-02-26 19:59:17 +0100

    refactor: Organize logic

--- app/src/main/java/com/sommerengineering/camerax/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/camerax/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.registerForActivityResult
+import androidx.camera.core.ImageCapture
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -17,15 +21,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import com.sommerengineering.camerax.ui.theme.CameraXTheme
+import java.util.concurrent.ExecutorService
 
 class MainActivity : ComponentActivity() {
+
+    // init camera
+    lateinit var imageCapture: ImageCapture
+    lateinit var videoCapture: VideoCapture<Recorder>
+    lateinit var recording: Recording
+    lateinit var executor: ExecutorService
+
+    // define permissions
+    private val requiredPermissions = mutableListOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.RECORD_AUDIO
+    ).toTypedArray()
 
     private val cameraPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
 
             var isPermissionsGranted = true
             permissions.entries.forEach {
-                if (it.key in requiredPermissions && it.value == false) {
+                if (it.key in requiredPermissions && !it.value) {
                     isPermissionsGranted = false
                 }
             }
@@ -33,26 +50,22 @@ class MainActivity : ComponentActivity() {
             if (!isPermissionsGranted) {
                 // permissions denied by user
             } else {
-                // start camera
+                startCamera()
             }
         }
-
-    private val requiredPermissions = mutableListOf(
-        android.Manifest.permission.CAMERA,
-        android.Manifest.permission.RECORD_AUDIO
-    ).toTypedArray()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        // ensure camera permission is granted
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            cameraPermissionRequest.launch(requiredPermissions)
+        // ensure all camera permission are granted
+        val isPermissionsGranted = requiredPermissions.all {
+            ContextCompat.checkSelfPermission(baseContext, it) ==
+                PackageManager.PERMISSION_GRANTED
         }
-
+        if (isPermissionsGranted) { startCamera() }
+        else { cameraPermissionRequest.launch(requiredPermissions) }
+        
         setContent {
             CameraXTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -63,6 +76,10 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    
+    fun startCamera() {
+        // todo
     }
 }
 
