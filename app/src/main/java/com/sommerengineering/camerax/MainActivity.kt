bcd81463ecf2476b13f10be64041410a60686b1c
Drew Sommer <drew@sommerengineering.com>
2025-02-26 20:16:39 +0100

    style: Compose layout

--- app/src/main/java/com/sommerengineering/camerax/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/camerax/MainActivity.kt
@@ -12,13 +12,22 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.video.Recorder
 import androidx.camera.video.Recording
 import androidx.camera.video.VideoCapture
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.sommerengineering.camerax.ui.theme.CameraXTheme
 import java.util.concurrent.ExecutorService
@@ -31,7 +40,6 @@ class MainActivity : ComponentActivity() {
     lateinit var recording: Recording
     lateinit var executor: ExecutorService
 
-    // define permissions
     private val requiredPermissions = mutableListOf(
         android.Manifest.permission.CAMERA,
         android.Manifest.permission.RECORD_AUDIO
@@ -58,7 +66,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        // ensure all camera permission are granted
+        // start camera, or request camera permissions
         val isPermissionsGranted = requiredPermissions.all {
             ContextCompat.checkSelfPermission(baseContext, it) ==
                 PackageManager.PERMISSION_GRANTED
@@ -66,16 +74,7 @@ class MainActivity : ComponentActivity() {
         if (isPermissionsGranted) { startCamera() }
         else { cameraPermissionRequest.launch(requiredPermissions) }
         
-        setContent {
-            CameraXTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContent { App() }
     }
     
     fun startCamera() {
@@ -84,19 +83,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App() {
+    CameraXTheme {
+        Surface (
+            modifier = Modifier.fillMaxSize()) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = 96.dp,
+                        bottom = 48.dp,
+                        start = 48.dp,
+                        end = 48.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                // image
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { }) {
+                    Text(
+                        text = "Capture image")
+                }
+
+                Spacer(
+                    modifier = Modifier.size(48.dp))
+
+                // video
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { }) {
+                    Text(
+                        text = "Capture video")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    CameraXTheme {
-        Greeting("Android")
-    }
+    App()
 }
