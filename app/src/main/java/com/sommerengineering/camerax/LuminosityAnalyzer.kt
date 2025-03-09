19cc4564c7cac0b3816db38e841bd1bd5878b5c4
Drew Sommer <drew@sommerengineering.com>
2025-03-09 21:25:14 +0100

    feat: Analyze average luminosity of image

--- app/src/main/java/com/sommerengineering/camerax/LuminosityAnalyzer.kt
+++ app/src/main/java/com/sommerengineering/camerax/LuminosityAnalyzer.kt
@@ -2,12 +2,28 @@ package com.sommerengineering.camerax
 
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import java.nio.ByteBuffer
 
 class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
 
-    
+    // convert byte array to byte buffer
+    private fun ByteBuffer.toByteArray() : ByteArray {
 
+        rewind()
+        val data = ByteArray(remaining())
+        get(data)
+        return data
+    }
+
+    // determine average luminosity of image
     override fun analyze(image: ImageProxy) {
-        TODO("Not yet implemented")
+
+        val buffer = image.planes[0].buffer
+        val data = buffer.toByteArray()
+        val pixels = data.map { it.toInt() and 0xFF }
+        val luma = pixels.average()
+
+        listener(luma)
+        image.close()
     }
 }
